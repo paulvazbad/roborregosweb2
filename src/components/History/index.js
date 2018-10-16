b515ab238697248ea43dff70dc51db844dc69033
Paul Vazquez <pev@live.com.mx>
2018-10-16 15:23:53 -0500

    random icon and random color for the lulz.

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import { AccessAlarm, ThreeDRotation, Build } from '@material-ui/icons';
+import { AccessAlarm, Polymer, Build, Code } from '@material-ui/icons';
 import 'react-vertical-timeline-component/style.min.css';
 import './styles.css';
 
@@ -11,8 +11,7 @@ export default class Timeline extends React.Component{
 			description: 'Desarrollo del prototipo de robot de servicio en la categoria @Home.',
 			image: 'https://via.placeholder.com/450x300',
 			members: ['Diego Garza', 'Emilio Chavez', 'Sebastian Rivera'],
-			date: '2018-present',
-			color: 'rgb(33, 150, 243)'
+			date: '2018-present'
 		},
 		{
 			title: 'LARC 2018',
@@ -20,24 +19,48 @@ export default class Timeline extends React.Component{
 			image: 'https://via.placeholder.com/450x300',
 			members: ['Osvaldo Alvarez', 'Alexis Virgen', 'Ivan Sol', 'Antonio Rodriguez', 'Mariano Uvalle'],
 			date: 'November 2018',
-			color: 'rgb(34, 100, 203)'
 		},
 		{
 			title: 'RobocupJr 2018',
 			description: 'Participacion en Robocup Jr en Montreal Canada con equipos de Soccer Open y Maze.',
 			image: 'https://via.placeholder.com/450x300',
 			members: ['Alberto Jahuey', 'Greg Espinoza', 'Yulisa Gutierrez', 'Alberto Lopez', 'Mariano Uvalle'],
-			date: 'November 2018',
-			color: 'rgb(34, 100, 203)'
+			date: 'November 2018'
+		},
+		{
+			title: 'TMR 2018',
+			description: 'Participacion en el Torneo Mexicano de Robotica en Monterrey, NL con equipos de Soccer Open, Soccer Light, Maze y Drones Autonomos',
+			image: 'https://via.placeholder.com/450x300',
+			members: ['Alberto Jahuey', 'Greg Espinoza', 'Yulisa Gutierrez', 'Alberto Lopez', 'Mariano Uvalle'],
+			date: 'November 2018'
 		}
 	]}
-
+getRandomColor = () =>{
+	let color;
+	color = Math.random();
+	console.log(color);
+	console.log(color.toString(16));
+	return Math.random().toString(16).slice(2, 8);
+}
+getRandomIcon = () =>  {
+  let ico = Math.floor(Math.random() * Math.floor(3));
+	console.log(ico);
+	if(ico===0){
+		return( <Build/>);
+	}
+	else if( ico===1){
+		return( <Code/>);
+	}
+	else{
+		return (<Polymer />);
+	}
+}
 	 returnTimeLineEle = () => {
 		return this.state.eventos.map( evento =>
 			 <VerticalTimelineElement
 				 className="vertical-timeline-element--work"
-				 iconStyle={{ background: evento.color, color: '#fff' }}
-				 icon={<Build />}
+				 iconStyle={{ background: "#" + this.getRandomColor(), color: '#fff' }}
+				 icon={this.getRandomIcon()}
 			 >
 				 <h3 className="vertical-timeline-element-title"><b>{evento.title}</b></h3>
 				 <h4 className="vertical-timeline-element-subtitle">{evento.date}</h4>
