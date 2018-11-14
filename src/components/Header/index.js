0b10676752272213bde7a39cf8f0be98edf459f9
José Eduardo Sánchez Yépiz <gallo.sanchez23@gmail.com>
2018-11-13 21:41:20 -0600

    Update src/components/Header/index.js
    Co-Authored-By: paulvazbad <pev@live.com.mx>

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from'react';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import MainLogo from '../../images/RoboLogo.png';
 import * as paths from '../../constants/paths.js'
 import './styles.css';
